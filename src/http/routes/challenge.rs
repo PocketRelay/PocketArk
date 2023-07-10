135884603f1edb08da7e9967b796eb8df8b77292
Jacobtread <jacobtread@gmail.com>
2023-07-10 16:32:09 +1200

    Static serving for challenges definitions

--- src/http/routes/challenge.rs
+++ src/http/routes/challenge.rs
@@ -1,6 +1,17 @@
+use axum::response::{IntoResponse, Response};
+use hyper::{header::CONTENT_TYPE, http::HeaderValue};
+use uuid::Uuid;
+
+static CHALLENGES: &str = include_str!("../../resources/defs/min/challenges.json");
 
 /// GET /challenges
-async fn get_challenges() {}
+async fn get_challenges() -> Response {
+    let mut resp = CHALLENGES.into_response();
+    resp.headers_mut()
+        .insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+
+    resp
+}
 
 /// GET /challenges/user
 async fn get_user_challenges() {}
