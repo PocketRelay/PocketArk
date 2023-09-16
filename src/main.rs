b67a5b4e529462d4d80d61a3a275f2082598af73
Jacobtread <jacobtread@gmail.com>
2023-09-16 15:53:20 +1200

    Migrated server to http removing openssl and axum-server deps

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 use crate::state::App;
+use crate::utils::constants::SERVER_PORT;
 use axum::Extension;
+use log::error;
 use log::LevelFilter;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
-use tokio::{select, signal};
-
-use crate::utils::constants::SERVER_PORT;
-use axum_server::tls_openssl::OpenSSLConfig;
-use log::error;
-use openssl::{
-    pkey::PKey,
-    rsa::Rsa,
-    ssl::{SslAcceptor, SslMethod},
-    x509::X509,
-};
+use tokio::signal;
 
 #[allow(unused)]
 mod blaze;
@@ -23,9 +15,6 @@ mod services;
 mod utils;
 
 mod state;
-
-const CERTIFICATE: &[u8] = include_bytes!("./resources/identity/cert.der");
-const PRIVATE_KEY: &[u8] = include_bytes!("./resources/identity/key.pem");
 
 #[tokio::main]
 async fn main() {
@@ -49,37 +38,13 @@ async fn main() {
         .layer(Extension(router))
         .layer(Extension(database));
 
-    let ssl_config = ssl_config();
-    let server_future =
-        axum_server::bind_openssl(addr, ssl_config).serve(router.into_make_service());
-    let close_future = signal::ctrl_c();
-
-    select! {
-        result = server_future => {
-            if let Err(err) = result {
-                error!("Failed to bind HTTP server on {}: {:?}", addr, err);
-                panic!();
-            }
-        }
-           // Handle the server being stopped with CTRL+C
-        _ = close_future => {}
+    if let Err(err) = axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .with_graceful_shutdown(async move {
+            _ = signal::ctrl_c().await;
+        })
+        .await
+    {
+        error!("Failed to bind HTTP server on {}: {:?}", addr, err);
     }
-}
-
-fn ssl_config() -> OpenSSLConfig {
-    let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls_server()).unwrap();
-
-    let crt = X509::from_der(CERTIFICATE).expect("Server certificate is invalid");
-    let pkey = PKey::from_rsa(
-        Rsa::private_key_from_pem(PRIVATE_KEY).expect("Server private key is invalid"),
-    )
-    .expect("Server private key is invalid");
-
-    acceptor
-        .set_certificate(&crt)
-        .expect("Failed to set HTTP server certificate");
-    acceptor
-        .set_private_key(&pkey)
-        .expect("Failed to set HTTP server private key");
-    OpenSSLConfig::try_from(acceptor).expect("Failed to create OpenSSL config")
 }
