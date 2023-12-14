be78e8a40715087a03c51187fd94d83af35d37b6
Jacobtread <jacobtread@gmail.com>
2023-12-14 19:18:42 +1300

    Cleaned up translation service, swapped translation map with int hashmap

--- src/services/i18n/mod.rs
+++ src/services/i18n/mod.rs
@@ -1,16 +1,22 @@
+//! Service that provides translations, translations are loaded from a .csv file
+//! at src/resources/data/i18n.csv
+
+use crate::utils::hashing::IntHashMap;
 use csv::ReaderBuilder;
 use log::debug;
-use std::collections::HashMap;
 
 const I18N_TRANSLATIONS: &[u8] = include_bytes!("../../resources/data/i18n.csv");
 
+/// Translation service
 pub struct I18nService {
-    pub map: HashMap<u32, String>,
+    /// Mapping between translation keys and the actual translation value
+    map: IntHashMap<u32, String>,
 }
 
 impl I18nService {
+    /// Creates a new i18n service
     pub fn new() -> Self {
-        let map: HashMap<u32, String> = ReaderBuilder::new()
+        let map: IntHashMap<u32, String> = ReaderBuilder::new()
             .from_reader(I18N_TRANSLATIONS)
             .into_records()
             .flatten()
@@ -27,6 +33,7 @@ impl I18nService {
         Self { map }
     }
 
+    /// Attempts to find a specific translation from its translation key
     pub fn get(&self, key: u32) -> Option<&str> {
         match self.map.get(&key) {
             Some(value) => Some(value),
