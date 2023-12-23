9f6fe06678f009ec77f86fb6075165a174f041ed
Jacobtread <jacobtread@gmail.com>
2023-12-23 19:39:02 +1300

    Created database migrations for new strike team tables

--- src/database/migration/m20231223_184934_create_strike_team_missions.rs
+++ src/database/migration/m20231223_184934_create_strike_team_missions.rs
@@ -6,43 +6,109 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(StrikeTeamMissions::Table)
                     .if_not_exists()
+                    // Unique ID of the strike team mission
                     .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
+                        ColumnDef::new(StrikeTeamMissions::Id)
+                            .unsigned()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    // The mission descriptor details
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::Descriptor)
+                            .json()
+                            .not_null(),
+                    )
+                    // The mission type details
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::MissionType)
+                            .json()
+                            .not_null(),
+                    )
+                    // Mission accessiblity
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::Accessibility)
+                            .unsigned()
+                            .not_null(),
+                    )
+                    // Custom defined mission waves
+                    .col(ColumnDef::new(StrikeTeamMissions::Waves).json().not_null())
+                    // Mission tags
+                    .col(ColumnDef::new(StrikeTeamMissions::Tags).json().not_null())
+                    // Static mission modifiers
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::StaticModifiers)
+                            .json()
+                            .not_null(),
+                    )
+                    // Dynamic mission modifiers
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::DynamicModifiers)
+                            .json()
+                            .not_null(),
+                    )
+                    // The mission rewarads
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::Rewards)
+                            .json()
+                            .not_null(),
+                    )
+                    // Custom attributes associated with the mission
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::CustomAttributes)
+                            .json()
+                            .not_null(),
+                    )
+                    // The time in seconds when the mission became available
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::StartSeconds)
+                            .unsigned()
+                            .not_null(),
+                    )
+                    // The time in seconds when the mission is no longer available
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::EndSeconds)
+                            .unsigned()
+                            .not_null(),
+                    )
+                    // The time in seconds the mission will take to complete (Strike teams)
+                    .col(
+                        ColumnDef::new(StrikeTeamMissions::SpLengthSeconds)
+                            .unsigned()
+                            .not_null(),
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(StrikeTeamMissions::Table).to_owned())
             .await
     }
 }
 
-/// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+pub enum StrikeTeamMissions {
     Table,
     Id,
-    Title,
-    Text,
+    Descriptor,
+    MissionType,
+    Accessibility,
+    Waves,
+    Tags,
+    StaticModifiers,
+    DynamicModifiers,
+    Rewards,
+    CustomAttributes,
+    StartSeconds,
+    EndSeconds,
+    SpLengthSeconds,
 }
--- src/database/migration/m20231223_185554_create_strike_team_mission_progress.rs
+++ src/database/migration/m20231223_185554_create_strike_team_mission_progress.rs
@@ -1,48 +1,94 @@
 use sea_orm_migration::prelude::*;
 
+use super::{
+    m20230714_105755_create_users::Users,
+    m20231223_184934_create_strike_team_missions::StrikeTeamMissions,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(StrikeTeamMissionProgress::Table)
                     .if_not_exists()
-                    .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
+                    // This table uses a composite key over the UserId and MissionId
+                    .primary_key(
+                        Index::create()
+                            .col(StrikeTeamMissionProgress::UserId)
+                            .col(StrikeTeamMissionProgress::MissionId),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(
+                        ColumnDef::new(StrikeTeamMissionProgress::UserId)
+                            .unsigned()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(StrikeTeamMissionProgress::MissionId)
+                            .unsigned()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(StrikeTeamMissionProgress::UserMissionState)
+                            .unsigned()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(StrikeTeamMissionProgress::Seen)
+                            .boolean()
+                            .not_null()
+                            .default(false),
+                    )
+                    .col(
+                        ColumnDef::new(StrikeTeamMissionProgress::Completed)
+                            .boolean()
+                            .not_null()
+                            .default(false),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(
+                                StrikeTeamMissionProgress::Table,
+                                StrikeTeamMissionProgress::UserId,
+                            )
+                            .to(Users::Table, Users::Id)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(
+                                StrikeTeamMissionProgress::Table,
+                                StrikeTeamMissionProgress::MissionId,
+                            )
+                            .to(StrikeTeamMissions::Table, StrikeTeamMissions::Id)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(
+                Table::drop()
+                    .table(StrikeTeamMissionProgress::Table)
+                    .to_owned(),
+            )
             .await
     }
 }
 
-/// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+enum StrikeTeamMissionProgress {
     Table,
-    Id,
-    Title,
-    Text,
+    MissionId,
+    UserId,
+    UserMissionState,
+    Seen,
+    Completed,
 }
